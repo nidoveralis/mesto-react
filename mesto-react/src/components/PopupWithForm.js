be974e89b9206d2fc25a85a1d1653366b3ae620e
Nidoveralis <nidoveralis@gmail.com>
2022-08-22 22:37:25 +0400

    feat: create PopupWithForm

---
+++ mesto-react/src/components/PopupWithForm.js
@@ -0,0 +1,28 @@
+
+
+function PopupWithForm({active, nameClass}) {
+  
+  console.log(active, nameClass)
+  return (
+    <></>
+   // <div className={`popup popup-${props.name}`}>
+     // <div className="popup__container">
+     // <button className="popup__button-close button" type="button"/>
+    //  <form className="popup__form form-profile" name={props.name} noValidate>
+      //  <h2 className="popup__title">{props.title}</h2>
+      //  <fieldset className="popup__field">
+        //  <input id="name-input" type="text" className="popup__input popup__input_type_name" name="name" placeholder="Имя" minLength="2" maxLength="40" required/>
+       //   <span className="name-input-error input-error"/>
+      //  </fieldset> 
+      ///  <fieldset className="popup__field">
+       //   <input  id="job-input" type="text" className="popup__input popup__input_type_job" name="about" placeholder="О себе" minLength="2" maxLength="200" required/>
+       //   <span className="job-input-error input-error"/> 
+       // </fieldset>
+      //  <input type="submit" value="Сохранить" className="popup__button-save"/>
+     // </form>
+    //  </div>
+   // </div>
+  )
+}
+
+export default PopupWithForm;
