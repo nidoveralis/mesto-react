40d8cce3359f57e121ad7fec879ce9897e64ceea
Nidoveralis <nidoveralis@gmail.com>
2022-08-23 13:54:02 +0400

    feat: PopupWithForm for editProfile and element

--- mesto-react/src/components/App.js
+++ mesto-react/src/components/App.js
@@ -9,14 +9,36 @@ function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
   const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
 
-  const childrenAvatar = 
-      <>
+  const childrenAvatar = <>
         <fieldset className="popup__field">
         <input  id="avatar" type="url" className="popup__input popup__input_type_avatar" name="avatar" minLength="2" maxLength="200" required />
         <span className="avatar-error input-error" /> 
         </fieldset>
         <input type="submit" value="Сохранить" className="popup__button-save popup__button-save_avatar" />
-      </>
+      </>;
+  const childrenProfile = <>
+    <fieldset className="popup__field">
+      <input id="name-input" type="text" className="popup__input popup__input_type_name" name="name" placeholder="Имя" minLength="2" maxLength="40" required />
+      <span className="name-input-error input-error" />
+    </fieldset> 
+    <fieldset className="popup__field">
+      <input  id="job-input" type="text" className="popup__input popup__input_type_job" name="about" placeholder="О себе" minLength="2" maxLength="200" required />
+      <span className="job-input-error input-error" /> 
+    </fieldset>
+    <input type="submit" value="Сохранить" className="popup__button-save" />
+  </>;
+  const childrenElement = <>
+  <fieldset className="popup__field">
+    <input id="name-element-input" type="text" className="popup__input popup__input_type_title" name="title" placeholder="Название" minLength="2" maxLength="30" required />
+    <span className="name-element-input-error input-error" /> 
+  </fieldset>
+  <fieldset className="popup__field">
+    <input id="link-element-input" type="url" className="popup__input popup__input_type_link" name="link" placeholder="Ссылка на картинку" required />
+    <span className="link-element-input-error input-error" /> 
+  </fieldset>
+  <input type="submit" value="Создать" className="popup__button-save" />
+  </>;
+
 
   function handleEditAvatarClick() {
     setIsEditAvatarPopupOpen(true);
@@ -37,70 +59,11 @@ function App() {
       <div className="page">
     <Header />
     <Main onEditAvatar = {handleEditAvatarClick} onEditProfile={handleEditProfileClick} onAddPlace ={handleAddPlaceClick} />
-    <PopupWithForm active = {isEditAvatarPopupOpen} name = {'popup-avatar'} title = {'Обновить аватар'} children = {childrenAvatar}/>
+    <PopupWithForm active = {isEditAvatarPopupOpen} name = {'avatar'} title = {'Обновить аватар'} children = {childrenAvatar}/>
+    <PopupWithForm active = {isEditProfilePopupOpen} name = {'profile'} title = {'Редактировать профиль'} children = {childrenProfile}/>
+    <PopupWithForm active = {isAddPlacePopupOpen} name = {'elements'} title = {'Новое место'} children = {childrenElement}/>
+    <PopupWithForm active = {false} name = {'deleteCard'} title = {'Вы уверены?'} children = {<input type="submit" value="Да" className="popup__button-save popup__button-save_delete" />}/>
     <Footer />
-
-    
-
-    <div className="popup popup-profile">
-      <div className="popup__container">
-        <button className="popup__button-close button" type="button" />
-        <form className="popup__form form-profile" name="personal-info" noValidate>
-          <h2 className="popup__title">Редактировать профиль</h2>
-          <fieldset className="popup__field">
-            <input id="name-input" type="text" className="popup__input popup__input_type_name" name="name" placeholder="Имя" minLength="2" maxLength="40" required />
-            <span className="name-input-error input-error" />
-          </fieldset> 
-          <fieldset className="popup__field">
-          <input  id="job-input" type="text" className="popup__input popup__input_type_job" name="about" placeholder="О себе" minLength="2" maxLength="200" required />
-          <span className="job-input-error input-error" /> 
-          </fieldset>
-          <input type="submit" value="Сохранить" className="popup__button-save" />
-        </form>
-      </div>
-    </div>
-
-    <div className="popup popup-avatar">
-      <div className="popup__container">
-        <button className="popup__button-close button" type="button" />
-        <form className="popup__form form-avatar" name="avatar" noValidate>
-          <h2 className="popup__title">Обновить аватар</h2>
-          <fieldset className="popup__field">
-          <input  id="avatar" type="url" className="popup__input popup__input_type_avatar" name="avatar" minLength="2" maxLength="200" required />
-          <span className="avatar-error input-error" /> 
-          </fieldset>
-          <input type="submit" value="Сохранить" className="popup__button-save popup__button-save_avatar" />
-        </form>
-      </div>
-    </div>
-
-    <div className="popup popup-deleteCard">
-      <div className="popup__container">
-        <button className="popup__button-close button" type="button" />
-        <form className="popup__form popup__form_dalete" name="delete-card" noValidate>
-          <h2 className="popup__title popup__title_delete">Вы уверены?</h2>
-          <input type="submit" value="Да" className="popup__button-save popup__button-save_delete" />
-        </form>
-      </div>
-    </div>
-
-    <div className="popup popup-elements">
-      <div className="popup__container">
-        <button className="popup__button-close button" type="button" />
-        <form className="popup__form form-element" name="new-element" noValidate>
-          <h2 className="popup__title">Новое место</h2>
-          <fieldset className="popup__field">
-            <input id="name-element-input" type="text" className="popup__input popup__input_type_title" name="title" placeholder="Название" minLength="2" maxLength="30" required />
-            <span className="name-element-input-error input-error" /> 
-          </fieldset>
-          <fieldset className="popup__field">
-            <input id="link-element-input" type="url" className="popup__input popup__input_type_link" name="link" placeholder="Ссылка на картинку" required />
-            <span className="link-element-input-error input-error" /> 
-          </fieldset>
-          <input type="submit" value="Создать" className="popup__button-save" />
-        </form>
-      </div>
-    </div>
     
     <div className="popup popup-picture">
       <div className="popup__container">
--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -3,10 +3,10 @@ function PopupWithForm({active, name, title, children}) {
 console.log(active);
 
   return(
-    <div className = {`${ active ? "popup_opened" : ""} popup  ${name}`} >
+    <div className = {`${ active ? "popup_opened" : ""} popup  popup-${name}`} >
       <div className="popup__container">
         <button className="popup__button-close button" type="button" />
-        <form className="popup__form form-profile" name={name} noValidate>
+        <form className={`popup__form form-${name}`} name={name} noValidate>
           <h2 className="popup__title">{title}</h2>
           {children}
         </form>
