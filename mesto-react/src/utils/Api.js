d715c5e0823c70be009adca309473d79c9414d7c
Nidoveralis <nidoveralis@gmail.com>
2022-08-23 16:32:51 +0400

    feat: create utils and api

---
+++ mesto-react/src/utils/Api.js
@@ -0,0 +1,103 @@
+import config from './utils'
+
+ class Api{
+  constructor(data) {
+    this._baseUrl = data.baseUrl;
+    this._headers = data.headers;
+  };
+
+  _getResponseData(res) {
+    if (!res.ok) {
+        return Promise.reject(`Ошибка: ${res.status}`); 
+    }
+    return res.json();
+  };
+
+  getUser() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    })
+    .then(res => this._getResponseData(res)
+    )
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers
+    })
+    .then(res => this._getResponseData(res))
+  };
+
+  editUser(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about
+      })
+    },
+    )
+    .then(res=>this._getResponseData(res))
+  };
+
+  addNewCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.title,
+        link: data.link
+      })
+    })
+    .then(res=>this._getResponseData(res))
+  };
+
+  deleteCard(data) {
+    return fetch(`${this._baseUrl}/cards/${data}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res=>this._getResponseData(res))
+  };
+
+  addlike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+    .then(res=>this._getResponseData(res))
+  };
+
+  deleteLike(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res=>this._getResponseData(res))
+  };
+
+  showAvatar() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    },
+    )
+    .then(res=>this._getResponseData(res))
+  };
+
+  editAvatar(data) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatar
+      })
+    },
+    )
+    .then(res=>this._getResponseData(res))
+  };
+};
+
+export const api = new Api(config);
+//console.log(api)
+//export api;
