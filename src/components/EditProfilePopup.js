9a473f2fdc4b60cf523bf9533fab36f106fe90de
Nidoveralis <nidoveralis@gmail.com>
2022-09-02 13:55:26 +0400

    fix: create useEffect

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import PopupWithForm from './PopupWithForm';
 
-function EditProfilePopup({isOpen, onClose, onUpdateUser}) {
+function EditProfilePopup({ onClose, isOpen, onUpdateUser}) {
 
   const currentUser = React.useContext(CurrentUserContext);
-  const [name, setName] = React.useState();
-  const [description, setDescription] = React.useState();
+  const [name, setName] = React.useState("");
+  const [description, setDescription] = React.useState("");
 
   React.useEffect(() => {
     setName(currentUser.name);
     setDescription(currentUser.about);
-  }, [currentUser]); 
+  }, [currentUser, isOpen]); 
 
   function nameInputValue(e) {
     setName(e.target.value);
@@ -31,11 +31,11 @@ function EditProfilePopup({isOpen, onClose, onUpdateUser}) {
 
   const childrenProfile = <>
     <fieldset className="popup__field">
-      <input id="name-input" type="text" className="popup__input popup__input_type_name" name="name" placeholder="Имя" minLength="2" maxLength="40" required onChange={nameInputValue} />
+      <input id="name-input" type="text" className="popup__input popup__input_type_name" value={name} name="name" placeholder="Имя" minLength="2" maxLength="40" required onChange={nameInputValue} />
       <span className="name-input-error input-error" />
     </fieldset> 
     <fieldset className="popup__field">
-      <input  id="job-input" type="text" className="popup__input popup__input_type_job" name="about" placeholder="О себе" minLength="2" maxLength="200" required onChange={nameInputAbout} />
+      <input  id="job-input" type="text" className="popup__input popup__input_type_job" value={description} name="about" placeholder="О себе" minLength="2" maxLength="200" required onChange={nameInputAbout} />
       <span className="job-input-error input-error" /> 
     </fieldset>
     <input type="submit" value="Сохранить" className="popup__button-save" />
